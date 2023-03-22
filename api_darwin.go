db61a3dff888701fccae37c273f00424eba15ec7
j178 <10510431+j178@users.noreply.github.com>
2023-03-22 21:42:04 +0800

    Separate darwin build

---
+++ api_darwin.go
@@ -0,0 +1,10 @@
+package tiktoken_go
+
+/*
+#cgo LDFLAGS: ./tiktoken-cffi/libtitoken.a -ldl -framework Security -framework Foundation
+#include <stdlib.h>
+
+extern unsigned int count_tokens(const char*, const char*);
+extern unsigned int get_context_size(const char*);
+*/
+import "C"
